e028aef554c602d75f8275736b98182ec4d8d68e
Stephen A. Davis <stephen@webdavis.io>
2024-10-28 12:46:55 -0600

    Create `FeedItem` module

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Stephen Davis on 2024-10-28.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
