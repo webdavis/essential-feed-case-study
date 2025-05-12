dc3a2df9993a8cf826daeb55fb98515f0d2ae8de
Stephen A. Davis <stephen@webdavis.io>
2025-05-11 19:49:51 -0600

    Improve error message on test failure

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -24,7 +24,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
       XCTAssertEqual(items[7], expectedItem(at: 7))
 
     case let .failure(error)?:
-      XCTFail("Expected successful feed result, got \(error) instead")
+      XCTFail("Expected successful feed result, got \(error) error instead")
       
     default:
       XCTFail("Expected successful feed result, got no result instead")
